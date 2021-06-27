765cb2fdee45d295804c006c977f1d6b063031b9
Marta Serheieva <marta_serheieva@epam.com>
2021-06-27 21:39:16 +0300

    Initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Meeting at school',
+    day: 'May 5th at 2:30 p.m.',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Food shopping',
+    day: 'May 6th at 1:30 p.m.',
+    reminder: true,
+  },
+]
